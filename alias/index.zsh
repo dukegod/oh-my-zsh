f97ebae1bcb2d0443498075082c1bc8a0dfe27d6
dukegod <dukehui1991@gmail.com>
2017-07-25 21:05:29 +0800

    add alias files to manage local aliases

---
+++ alias/index.zsh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# ---
+# 修改默认快捷键
+# ---
+alias lla="ls -al"
+
+# ---
+# npm list top level modules
+# ---
+alias ng="npm list -g --depth=0 2>/dev/null"
+alias nl="npm list --depth=0 2>/dev/null"
+
+# ---
+# nginx
+# ---
+alias ngxps="ps -ef | grep nginx"
+
+# ---
+# mysql
+# ---
+alias mysql="mysql -u root -p"
+
+# ---
+# archey
+# ---
+alias ay="archey"
