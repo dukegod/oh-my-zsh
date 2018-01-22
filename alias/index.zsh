303bf7c99767a0a533374214ddd752b990588b25
dukegod <dukehui1991@gmail.com>
2018-01-22 14:15:03 +0800

    chrome

--- alias/index.zsh
+++ alias/index.zsh
@@ -9,6 +9,12 @@
 # ---
 #alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 
+#---
+#alias chrome
+#---
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+alias chromeca="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
+
 # ---
 # npm list top level modules
 # ---
